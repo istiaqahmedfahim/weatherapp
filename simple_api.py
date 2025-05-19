fba7fbebc9acf725c87591ae3ab52e5989ea46ec
istiaqahmedfahim <istiakahmed064@gmail.com>
2025-05-19 13:07:04 +0600

    fixed

--- simple_api.py
+++ simple_api.py
@@ -11,7 +11,7 @@ city = st.text_input("Enter city name:")
 if st.button("Get Weather"):
     if city:
         # Replace this with your OpenWeatherMap API key
-        API_KEY = "91693b637b61ecc201908a21156af97a"
+        API_KEY = ""
         URL = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
 
         # Request to the API
